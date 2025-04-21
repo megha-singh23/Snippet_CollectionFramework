b3e216c8450156f44360830c4497dd6ce89d3d66
Megha Singh <meghashakya235@gmail.com>
2025-04-21 20:09:13 +0530

    Learn ArrayDeque

--- src/Queue/Deque/ArrayDequeTest.java
+++ src/Queue/Deque/ArrayDequeTest.java
@@ -1,8 +1,52 @@
 package Queue.Deque;
 
 import java.util.ArrayDeque;
+import java.util.Iterator;
 
 public class ArrayDequeTest {
     
-    ArrayDeque<Integer> arr=new ArrayDeque<>();
+    public static void main(String[] args) {
+
+        ArrayDeque<Integer> arr=new ArrayDeque<>();
+        arr.add(23);
+        arr.add(67);
+        arr.add(78);
+        arr.add(11);
+        arr.add(87);
+
+        arr.forEach(System.out::println);
+
+        arr.addFirst(55);
+        arr.addLast(12);
+
+        System.out.println(arr.equals(arr));
+
+        System.out.println(arr.getClass());
+
+        System.out.println(arr.offerFirst(23));
+
+        System.out.println(arr);
+
+        System.out.println(arr.remove(23));
+
+        System.out.println(arr.size());
+
+        System.out.println(arr.toString());
+
+        System.out.println(arr.removeIf(e-> e>=15));
+
+        System.out.println(arr.isEmpty());
+
+        System.out.println(arr.poll());
+
+        Iterator<Integer> a=arr.iterator();
+        while(a.hasNext()){
+            System.out.println(a.next());
+        }
+
+        arr.clear();
+        
+    }
+
+    
 }
